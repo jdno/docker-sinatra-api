809c01d5831b2ce015f8b1c693725febaffa2b7a
Jan David <jandavid@awesometechnology.de>
2016-02-08 03:07:13 +0100

    Extend app to make use of ActiveRecord
    The app has been extended to make use of the database and ActiveRecord.
    To be a useful example, CRUD operations have been implemented for the
    API allowing users to view, create, update and delete resources from the
    database.

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,53 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'sinatra/activerecord'
+
+set :database_file, 'config/database.yml'
+
+class Resource < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+end
 
 get '/' do
-  json 'Hello World!'
+  json Resource.select('id', 'name').all
+end
+
+get '/:id' do
+  resource =  Resource.find_by_id(params[:id])
+
+  if resource
+    json resource
+  else
+    halt 404
+  end
+end
+
+post '/' do
+  resource = Resource.create(params)
+
+  if resource
+    halt 206, json(resource)
+  else
+    halt 500
+  end
+end
+
+patch '/:id' do
+  resource = Resource.find_by_id(params[:id])
+
+  if resource
+    resource.update(name: params[:name])
+  else
+    halt 404
+  end
+end
+
+delete '/:id' do
+  resource = Resource.find_by_id(params[:id])
+
+  if resource
+    resource.destroy
+  else
+    halt 404
+  end
 end
